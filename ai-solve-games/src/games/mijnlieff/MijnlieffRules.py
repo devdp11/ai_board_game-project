355c089f8bade540477cd2d370e55575931543af
Eldiley  Teles (Ley) <eldileyamado@gmail.com>
2023-04-19 11:31:50 +0100

    alterando numeros de linha e coluna

--- ai-solve-games/src/games/mijnlieff/MijnlieffRules.py
+++ ai-solve-games/src/games/mijnlieff/MijnlieffRules.py
@@ -1,17 +1,27 @@
 from games.mijnlieff.MijnlieffAction import MijnlieffAction
+from games.mijnlieff.MijnlieffState import MijnlieffState
  
 class MijnlieffRules():
 
-    def __display_Rules(self):
-        print("1 | Peça  Straights")
-        print("2 | Peça  Diagonals")
-        print("3 | Peça  Pushers")
-        print("4 | Peça  Pullers")
+    def __display_Rules(self, row, col):
+        print({
+            0: 'Peça  Straights',
+            1: 'Peça  Diagonals',
+            3: 'Peça  Pushers',
+            4: 'Peça  Pullers',
+            MijnlieffState.EMPTY_CELL: ' '
+        }[self.__grid[row][col]], end="")
+        
         choice = int(input("Escolha uma peça para jogar:"))
 
 
     def __display_Piece(self, row, col):
-        
+        self.__num_rows = row
+        self.__num_cols = col
+        for row in range(self.__num_rows):
+            for col in range(self.__num_cols):
+                if player == 1:
+                    
         print([])
         pass
 
